135389f027e74541c9d28e6cad2e69c38d665a99
Xamuel Wekobosya <samweko@gmail.com>
2020-11-29 19:49:03 +0300

    starting

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/qsacco", (req, res) => {
+  res.set("Cache-Control", "public, max-age=300 s-maxage=600");
+  res.send(`Date is: ${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
